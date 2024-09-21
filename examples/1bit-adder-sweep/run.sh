d9b67152cce454ac01b0c1a00ab0724309e0684a
deyuan <guodeyuan@gmail.com>
2024-09-21 08:06:15 +0000

    Add two example sh files for 1bit and 2bit adder num register sweeping.

---
+++ examples/1bit-adder-sweep/run.sh
@@ -0,0 +1,29 @@
+
+apptainer exec ../../myapptainer.sif ../../bit_serial_compiler.py \
+    --verilog ../../src/verilog/1bit_full_adder.v \
+    --genlib ../../src/genlibs/inv_and_xnor_mux.genlib \
+    --output 2reg \
+    --outdir outputs \
+    --num-regs 2
+
+apptainer exec ../../myapptainer.sif ../../bit_serial_compiler.py \
+    --verilog ../../src/verilog/1bit_full_adder.v \
+    --genlib ../../src/genlibs/inv_and_xnor_mux.genlib \
+    --output 3reg \
+    --outdir outputs \
+    --num-regs 3
+
+apptainer exec ../../myapptainer.sif ../../bit_serial_compiler.py \
+    --verilog ../../src/verilog/1bit_full_adder.v \
+    --genlib ../../src/genlibs/inv_and_xnor_mux.genlib \
+    --output 4reg \
+    --outdir outputs \
+    --num-regs 4
+
+apptainer exec ../../myapptainer.sif ../../bit_serial_compiler.py \
+    --verilog ../../src/verilog/1bit_full_adder.v \
+    --genlib ../../src/genlibs/inv_and_xnor_mux.genlib \
+    --output 5reg \
+    --outdir outputs \
+    --num-regs 5
+
