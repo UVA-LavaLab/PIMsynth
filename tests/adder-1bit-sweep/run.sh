f7895e8ef0ad317d74667757cbdee37ae9a0f238
deyuan <guodeyuan@gmail.com>
2024-09-25 06:07:03 +0000

    Update adder test run scripts.

--- tests/adder-1bit-sweep/run.sh
+++ tests/adder-1bit-sweep/run.sh
@@ -1,26 +1,26 @@
 
-apptainer exec ../../myapptainer.sif ../../bit_serial_compiler.py \
+../../apptainer-run.sh ../../bit_serial_compiler.py \
     --verilog ../../src/verilog/full_adder_1bit.v \
     --genlib ../../src/genlibs/inv_and_xnor_mux.genlib \
     --output 2reg \
     --outdir outputs \
     --num-regs 2
 
-apptainer exec ../../myapptainer.sif ../../bit_serial_compiler.py \
+../../apptainer-run.sh ../../bit_serial_compiler.py \
     --verilog ../../src/verilog/full_adder_1bit.v \
     --genlib ../../src/genlibs/inv_and_xnor_mux.genlib \
     --output 3reg \
     --outdir outputs \
     --num-regs 3
 
-apptainer exec ../../myapptainer.sif ../../bit_serial_compiler.py \
+../../apptainer-run.sh ../../bit_serial_compiler.py \
     --verilog ../../src/verilog/full_adder_1bit.v \
     --genlib ../../src/genlibs/inv_and_xnor_mux.genlib \
     --output 4reg \
     --outdir outputs \
     --num-regs 4
 
-apptainer exec ../../myapptainer.sif ../../bit_serial_compiler.py \
+../../apptainer-run.sh ../../bit_serial_compiler.py \
     --verilog ../../src/verilog/full_adder_1bit.v \
     --genlib ../../src/genlibs/inv_and_xnor_mux.genlib \
     --output 5reg \
