9571645ab03921d4dd5edb9b77f4d20540c7f613
mhgholamrezaei <mgh27.1997@gmail.com>
2024-10-07 18:25:24 -0400

    Update AES sbox

--- src/verilog/mul_int32.v
+++ src/verilog/mul_int32.v
@@ -1,7 +1,7 @@
 // int32 mul
 // deyuan, 09/25/2024
 
-module mul_int32(input [3:0] pi0, input [3:0] pi1, output [3:0] po0);
+module mul_int32(input [31:0] pi0, input [31:0] pi1, output [31:0] po0);
     assign po0 = pi0 * pi1;
 endmodule
 
