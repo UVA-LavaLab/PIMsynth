a5e5ff708bf169a3cb598b7d91ed9962f7e54bd3
mhgholamrezaei <mgh27.1997@gmail.com>
2024-10-24 20:02:50 -0400

    Set test enviornment variables

---
+++ set-envvars.sh
@@ -0,0 +1,4 @@
+export PIMEVAL_CONFIG_PATH=/u/uab9qt/worktable/PIMeval-PIMbench/configs
+export PIMEVAL_CONFIG_SIM=PIMeval_BitSimdV.cfg
+export LIB_PIMEVAL_PATH=/u/uab9qt/worktable/PIMeval-PIMbench/libpimeval
+
