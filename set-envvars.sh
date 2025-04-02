e40e75898c4ad6e2981458f501874c27d4f997b4
deyuan <guodeyuan@gmail.com>
2025-04-02 09:56:24 +0000

    Update set-envvars.sh for PIMeval.

--- set-envvars.sh
+++ set-envvars.sh
@@ -1,11 +1,9 @@
 
 ROOT=$(git rev-parse --show-toplevel)
 
-export PIMEVAL_CONFIG_PATH=$ROOT/PIMeval-PIMbench/configs
-export PIMEVAL_CONFIG_SIM=PIMeval_BitSimdV.cfg
+export PIMEVAL_SIM_CONFIG=$ROOT/PIMeval-PIMbench/configs/PIMeval_BitSimdV.cfg
 export LIB_PIMEVAL_PATH=$ROOT/PIMeval-PIMbench/libpimeval
 
 echo "ROOT : $ROOT"
-echo "PIMEVAL_CONFIG_PATH : $PIMEVAL_CONFIG_PATH"
-echo "PIMEVAL_CONFIG_PATH : $PIMEVAL_CONFIG_SIM"
+echo "PIMEVAL_SIM_CONFIG : $PIMEVAL_SIM_CONFIG"
 
