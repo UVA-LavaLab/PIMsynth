6381329e68ce6ed787483a289be9afad00eadf9f
deyuan <guodeyuan@gmail.com>
2025-04-16 11:53:06 +0000

    Fix apptainer-run.sh when ./ prefix is missing.

--- apptainer-run.sh
+++ apptainer-run.sh
@@ -3,5 +3,10 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
+# Add ./ prefix to the first file argument
+if [[ -f "$1" && "$1" != /* ]]; then
+	set -- "./$1" "${@:2}"
+fi
+
 apptainer exec $SCRIPT_DIR/myapptainer.sif "$@"
 
