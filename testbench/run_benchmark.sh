5e831d0c8b8f3f923221b100beabe20ce6802fed
deyuan <guodeyuan@gmail.com>
2025-04-20 11:10:12 +0000

    Update run_benchmark.sh for configurability.

--- testbench/run_benchmark.sh
+++ testbench/run_benchmark.sh
@@ -6,8 +6,13 @@
 # CREATED: 03/28/2025
 #===============================================================================
 
+# NOTE: Use apptainer to run this script. Do not call apptainer in this script.
+
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-PROJ_ROOT="$(realpath "$SCRIPT_DIR/..")"
+PROJ_ROOT=$(git rev-parse --show-toplevel)
+SUBMODULE_DIR=$PROJ_ROOT/benchmarks
+# Note: Copy this script to another directory and set VERILOG_DIR to ./ for testing
+VERILOG_DIR=$PROJ_ROOT/benchmarks
 
 # Define a list of valid bit-serial ISA
 VALID_BIT_SERIAL_ISA=(
@@ -151,11 +156,11 @@ verilog_files=()
 while IFS= read -r line || [ -n "$line" ]; do
     # Skip empty lines and comments
     [[ -z "$line" || "$line" =~ ^# ]] && continue
-    verilog_files+=("$PROJ_ROOT/benchmarks/$line")
-done < "$PROJ_ROOT/benchmarks/submodule_list.txt"
+    verilog_files+=("$SUBMODULE_DIR/$line")
+done < "$SUBMODULE_DIR/submodule_list.txt"
 
 # Add top-level benchmark file
-verilog_files+=("$PROJ_ROOT/benchmarks/${benchmark_name}.v")
+verilog_files+=("$VERILOG_DIR/${benchmark_name}.v")
 
 # Check if verilog files exist
 for verilog_file in "${verilog_files[@]}"; do
@@ -166,7 +171,7 @@ for verilog_file in "${verilog_files[@]}"; do
 done
 
 # Call the bit-serial compiler
-$PROJ_ROOT/apptainer-run.sh $PROJ_ROOT/bit_serial_compiler.py \
+$PROJ_ROOT/bit_serial_compiler.py \
     --verilog "${verilog_files[@]}" \
     --genlib "$genlib_file" \
     --num-regs "$num_reg" \
@@ -176,9 +181,9 @@ $PROJ_ROOT/apptainer-run.sh $PROJ_ROOT/bit_serial_compiler.py \
 
 # Make the test
 cd "./outputs__$target"
-$PROJ_ROOT/apptainer-run.sh make
+make
 
 # Run the test
-$PROJ_ROOT/apptainer-run.sh ./${target}.test.out
+./${target}.test.out
 cd ..
 
