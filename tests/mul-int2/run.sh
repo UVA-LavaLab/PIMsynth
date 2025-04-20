cdfc947d993b287bf00dc3ce0d795aa92e72c076
deyuan <guodeyuan@gmail.com>
2025-04-20 12:24:39 +0000

    Create a main function to test mul-int2 bit-wise C code.

--- tests/mul-int2/run.sh
+++ tests/mul-int2/run.sh
@@ -7,3 +7,4 @@ PROJ_ROOT=$(git rev-parse --show-toplevel)
 #$PROJ_ROOT/apptainer-run.sh $SCRIPT_DIR/run_benchmark.sh inv_nand 4 mul_int2
 $PROJ_ROOT/apptainer-run.sh $SCRIPT_DIR/run_benchmark.sh inv_and_xnor_mux 4 mul_int2
 
+gcc test_bitwise.c outputs__inv_and_xnor_mux__4__mul_int2/inv_and_xnor_mux__4__mul_int2.bitwise.c -o test_bitwise.out
