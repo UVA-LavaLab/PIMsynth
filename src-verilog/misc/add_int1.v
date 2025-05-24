79ade0d0dcc44941b3d08162dc6b95da64d67f68
deyuan <guodeyuan@gmail.com>
2025-05-24 20:00:25 +0000

    Add add/mul int1/2 test cases under misc folder.

---
+++ src-verilog/misc/add_int1.v
@@ -0,0 +1,19 @@
+// 1-bit Integer Addition
+// Dependencies: adder_1bit.v adder_nbit.v
+// deyuan, 05/24/2025
+
+module add_int1 #(
+    parameter WIDTH = 1
+)(
+    input [WIDTH-1:0] A,
+    input [WIDTH-1:0] B,
+    output [WIDTH-1:0] Sum
+);
+    adder_nbit #(
+        .WIDTH(WIDTH)
+    ) u_adder_nbit (
+        .A(A),
+        .B(B),
+        .Sum(Sum)
+    );
+endmodule
---
+++ src-verilog/misc/add_int2.v
@@ -0,0 +1,19 @@
+// 2-bit Integer Addition
+// Dependencies: adder_1bit.v adder_nbit.v
+// deyuan, 05/24/2025
+
+module add_int2 #(
+    parameter WIDTH = 2
+)(
+    input [WIDTH-1:0] A,
+    input [WIDTH-1:0] B,
+    output [WIDTH-1:0] Sum
+);
+    adder_nbit #(
+        .WIDTH(WIDTH)
+    ) u_adder_nbit (
+        .A(A),
+        .B(B),
+        .Sum(Sum)
+    );
+endmodule
