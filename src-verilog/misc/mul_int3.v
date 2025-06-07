84f3a12e3bc95e091881db557c9da688c6aaa6dc
deyuan <guodeyuan@gmail.com>
2025-06-07 18:55:25 +0000

    Add mul_in3 and mul_int4 testcases.

---
+++ src-verilog/misc/mul_int3.v
@@ -0,0 +1,20 @@
+// 3-bit Integer Multiplication
+// Dependencies: multiplier_nbit.v adder_1bit.v adder_nbit.v
+// deyuan, 05/24/2025
+
+module mul_int3 #(
+    parameter WIDTH = 3
+)(
+    input  [WIDTH-1:0] A,  // Multiplicand
+    input  [WIDTH-1:0] B,  // Multiplier
+    output [WIDTH-1:0] P   // Lower WIDTH bits of the product
+);
+
+    // Instantiate the n-bit multiplier submodule
+    multiplier_nbit #(WIDTH) u_multiplier_nbit (
+        .A(A),
+        .B(B),
+        .P(P)
+    );
+
+endmodule
